bc032b2052682812ef26f6bccae424353df16b28
ClancyR <clancy.rye@gmail.com>
2018-10-04 20:32:33 +1000

    Removed main entrypoint, wrote code for init_db and rm_db

--- run.py
+++ run.py
@@ -1,27 +1,96 @@
 # run.py
 #
-# This file is our entrypoint for the web application. Its main responsibility
+# This file is our entry-point for the web application. Its main responsibility
 # is as a bootstrap script to instantiate and configure our Flask web app.
 #
 # It has a secondary function of configuring our Flask shell so we're able to
-# use the 'flask shell' command to easily have access to our database and user
-# contexts.
+# use the 'flask shell' command to easily access and interact with our database
 #
-# The method this file uses to instantiate and configure our Flask web app is a
-# little hidden - it achieves this by importing app, which loads
-# app/__init__.py, which contains the actual instantiation and configuration.
-# :)
+# As a note - this file instantiates and configures our Flask web app by importing 'app', loading app/__init__.py,
+# which contains the actual instantiation and configuration. :)
 
+import logging
+from os import remove, makedirs
+from shutil import rmtree
+from subprocess import call
 
-#--------------------------------------------------------------------#
-#                        HEY - STOP RIGHT NOW                        #
-#--------------------------------------------------------------------#
+from termcolor import colored
 
-# Don't call this script directly. Instead, use 'flask run'.
+from app import app
+from app.health_care_system import HealthCareSystem
+from app.models import *
 
-from app import app, db
-from app.models import User
+logger = logging.getLogger(__name__)
+
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User}
+    """
+    Runs when 'flask shell' is invoked - adds database instance and models to shell session.
+
+    :return: None
+    """
+    return {'db': db, 'User': User, 'WorksAt': WorksAt, 'Centre': Centre}
+
+
+@app.cli.command('load_from_csv')
+def load_from_csv():
+    """
+    Runs when 'flask load_from_csv' is invoked - enters all information from provided CSV files.
+
+    :return: None
+    """
+
+    # We're required to do this, as UserManager is a property of HSC. A little annoying and hacky. :'(
+    hsc = HealthCareSystem()
+    hsc.load_from_csv()
+
+
+@app.cli.command('init_db')
+def init_database_command():
+    """
+    Runs when 'flask init_db' is invoked - prepares the database, including:
+        - Initialising the database
+        - Performing database migrations
+        - Performing database upgrades
+        - Loading CSV data into database
+        - Flagging database upgrade as complete
+
+    :return: None
+    """
+    logger.debug(colored("Initialising database.", "yellow"))
+    call('flask db init', shell=True)
+
+    logger.debug(colored("Applying database migrations.", "yellow"))
+    call('flask db migrate', shell=True)
+
+    logger.debug(colored("Upgrading database.", "yellow"))
+    call('flask db upgrade', shell=True)
+
+    logger.debug(colored("Flagging upgrades complete.", "yellow"))
+    call('touch migrations/versions/applied', shell=True)
+
+    logger.debug(colored("Loading data from CSV files.", "yellow"))
+    call('flask load_from_csv', shell=True)
+
+    logger.debug(colored("Database initalised.", "green"))
+
+
+@app.cli.command('rm_db')
+def remove_database_command():
+    """
+    Runs when 'flask rm_db' is invoked - removes the database and migrations/versions folder.
+
+    :return: None
+    """
+    logger.debug(colored("Removing database and migrations.", "yellow"))
+    try:
+        # Remove database and all recorded migrations before recreating empty migrations/versions folder.
+        # This 'resets' the database. I selectively delete versions rather than migrations as I want to return the
+        # migration logging configuration stored within migrations.
+        remove('./app/app.db')
+        rmtree('./migrations/versions')
+        makedirs('./migrations/versions')
+        logger.debug(colored("Database and migrations/versions removed.", "green"))
+    except Exception as e:
+        logger.error(str(e))
