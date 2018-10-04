5c90965591bd1e5a4414d52c7ec25b9613f8340a
ClancyR <clancy.rye@gmail.com>
2018-10-04 21:22:25 +1000

    Reformatted with pep8, optimised imports. Also moved login and logout routines to UserManager. Solved world hunger. You go, girl!

--- app/routes.py
+++ app/routes.py
@@ -2,51 +2,60 @@
 #
 # This file defines the different URLs (routes) our webserver implements.
 # Handlers for each of these routes, known as views, are also defined here.
+#
+# Yeahhh boiiii - das my guy - Clancy Rye
 
-from flask import flash, redirect, render_template, request, url_for
-from flask_login import current_user, login_user, login_required, logout_user
-from app import app, db
-from app.forms import LoginForm, RegistrationForm
-from app.models import User
-from werkzeug.urls import url_parse
-
-from app.user import *
-from app.appointment import *
-from app.centre import *
 import csv
 import datetime
+import os
 
-#This contains temp info from .csv files
+from flask import flash, redirect, render_template, request, url_for
+from flask_login import current_user, login_required
+
+from app import app
+from app.appointment import *
+from app.centre import *
+from app.forms import LoginForm, RegistrationForm
+from app.health_care_system import HealthCareSystem
+from app.user import *
+
+# Dirty dirty hacky yucky smucky - basically makes sure database has been upgraded once.
+# If we don't do this, when Flask is in debug mode, performing flask db init will make flask import routes.py, which
+# will initialise a HealthCareSystem, which attempts to load data from the database when being initialised.
+#
+# As we have obviously not finished initalising the database, it'll error out from trying to use the database it's in
+# the process of setting up.
+if os.path.isfile('./migrations/versions/applied'):
+    hsc = HealthCareSystem()
+
+# This contains temp info from .csv files
 centreList = []
 providerList = []
-user1 = patient(full_name = "andy", email_address = "andy@gmail.com")
-user2 = patient(full_name = "james", email_address = "james@gmail.com")
-user3 = health_care_provider(full_name = "jessica", email_address = "jessica@gmail.com", isprovider = 1)
-
+user1 = patient(full_name="andy", email_address="andy@gmail.com")
+user2 = patient(full_name="james", email_address="james@gmail.com")
+user3 = health_care_provider(full_name="jessica", email_address="jessica@gmail.com", isprovider=1)
 
 currUser = user1
-
 
 with open('app/static/data/health_centres.csv') as f:
     reader = csv.DictReader(f)
     for row in reader:
         centreType = row['centre_type']
-        centreName = row['name'] 
+        centreName = row['name']
         centrePhone = row['phone']
-        centreID = row['abn'] 
+        centreID = row['abn']
         centreSub = row['suburb']
-        centre = health_care_centre(centreName, centreSub, centrePhone, type = centreType)
+        centre = health_care_centre(centreName, centreSub, centrePhone, type=centreType)
         centreList.append(centre)
-        
 
 with open('app/static/data/provider.csv') as g:
     reader = csv.DictReader(g)
     for row in reader:
         email = row['provider_email']
         type = row['provider_type']
-        pw = row['password']  
-        provider = health_care_provider(email_address = email,type = type)
-        providerList.append(provider)  
+        pw = row['password']
+        provider = health_care_provider(email_address=email, type=type)
+        providerList.append(provider)
 
 with open('app/static/data/provider_health_centre.csv') as g:
     reader = csv.DictReader(g)
@@ -54,23 +63,24 @@ with open('app/static/data/provider_health_centre.csv') as g:
     for row in reader:
         email = row['provider_email']
         centre = row['health_centre_name']
-        #assign the class centre rather than the string
+        # assign the class centre rather than the string
         for c in centreList:
-            if (centre == c._name): 
+            if (centre == c._name):
                 centreClass = c
-        #similarly assign the class provider rather than the email
+        # similarly assign the class provider rather than the email
         for p in providerList:
             if (email == p._email_address):
                 providerClass = p
 
-        for provider in providerList:    
+        for provider in providerList:
             if (provider._email_address == email):
-                provider.addCentre(centreClass)     #add centre to provider
+                provider.addCentre(centreClass)  # add centre to provider
 
-        for c in centreList: # add provider to centre
+        for c in centreList:  # add provider to centre
             if (c._name == centre):
                 c.addProvider(providerClass)
-                #c.addProvider(email[0:email.find('@')])
+                # c.addProvider(email[0:email.find('@')])
+
 
 @app.route('/')
 @login_required
@@ -82,30 +92,30 @@ def index():
     This is loaded when no route is specified in the URL, or when redirected to
     as a 'safety net'.
     """
-    
-    return render_template('index.html', title='home', user = currUser)
+
+    return render_template('index.html', title='home', user=currUser)
 
 
-@app.route('/booking', methods = ['GET', 'POST'])
+@app.route('/booking', methods=['GET', 'POST'])
 def booking():
     doneBooking = 0
     now = str(datetime.datetime.now())
     date = now[0:now.find(" ")]
-    time = now [now.find(" ") + 1:now.find(".") - 3]
+    time = now[now.find(" ") + 1:now.find(".") - 3]
     app = ""
     if (request.method == "POST"):
         book = int(request.form["book"])
         # parameters for search
-        c = request.form["c"] # search for centres 
-        p = request.form["p"] # search for providers
-        search = request.form["search"] # search input
-        provider = request.form['provider'] # provider currently being booked for
+        c = request.form["c"]  # search for centres
+        p = request.form["p"]  # search for providers
+        search = request.form["search"]  # search input
+        provider = request.form['provider']  # provider currently being booked for
 
         for prov in providerList:
             if (prov._full_name == provider):
-                providerClass = prov # returns the instance of provider
+                providerClass = prov  # returns the instance of provider
 
-        if (book): # if we are booking
+        if (book):  # if we are booking
 
             # set date/time/centre
 
@@ -113,9 +123,9 @@ def booking():
             centre = request.form["centre"]
             time = str(request.form["time"])
             length = int(request.form["length"])
-        
+
             # convert time to minutes then add on the legnth of appointment and convert back to time
-            totalLen = timeToMin(time) + length       
+            totalLen = timeToMin(time) + length
             timeEnd = minToTime(totalLen)
 
             print("curent appointment starts:" + time + " ends:" + timeEnd)
@@ -123,19 +133,22 @@ def booking():
             for app in providerClass._appointment_list:
                 clash = timeClash(time, app._start_time, timeEnd, app._end_time)
                 if (clash):
-                    return render_template('booking.html', user = currUser, c = c, p = p, search = search, \
-                           provider = providerClass, book = -1, t = time, d = date, app = app)
-                
-            if (date == ""):  #just for testing, this should never happen
-                return render_template('booking.html', user = currUser, c = c, p = p, search = search, provider = providerClass, noDate = 1)
-            
-            app = appointment(start_time = time,end_time = timeEnd, date = date, patient = currUser,health_care_provider = providerClass, centre = centre)
+                    return render_template('booking.html', user=currUser, c=c, p=p, search=search, \
+                                           provider=providerClass, book=-1, t=time, d=date, app=app)
+
+            if (date == ""):  # just for testing, this should never happen
+                return render_template('booking.html', user=currUser, c=c, p=p, search=search, provider=providerClass,
+                                       noDate=1)
+
+            app = appointment(start_time=time, end_time=timeEnd, date=date, patient=currUser,
+                              health_care_provider=providerClass, centre=centre)
             doneBooking = 1
 
-    return render_template('booking.html', user = currUser, c = c, p = p, search = search, provider = providerClass, book = doneBooking, t = time, d = date, app = app)
+    return render_template('booking.html', user=currUser, c=c, p=p, search=search, provider=providerClass,
+                           book=doneBooking, t=time, d=date, app=app)
 
 
-@app.route('/profile/<c>', methods = ['POST', 'GET'])
+@app.route('/profile/<c>', methods=['POST', 'GET'])
 def profile(c):
     print("HEREHREHREHRHERHEH")
     if (request.method == "POST"):
@@ -146,63 +159,59 @@ def profile(c):
 
         for a in providerList:
             if (a._email_address == text):
-                return render_template('profile.html', object = a, c = c, p = p, search = search)
-        
+                return render_template('profile.html', object=a, c=c, p=p, search=search)
+
         print("search is " + search)
         for centre in centreList:
             if (text == centre._name):
-                return render_template('profile.html', object = centre, c = c, p = p, search = search)
+                return render_template('profile.html', object=centre, c=c, p=p, search=search)
+
+        apple = health_care_centre("asdf", "asdf")
+        return render_template('profile.html', object=apple, c=c, p=p, search=search)
 
 
-        apple = health_care_centre("asdf","asdf")
-        return render_template('profile.html', object = apple, c = c, p = p, search = search)
-
-@app.route('/search', methods = ['GET', 'POST'])
+@app.route('/search', methods=['GET', 'POST'])
 def search():
-
     print("DSJKFLKJLDSF")
-    if (request.method == 'POST'): #redirect to the search screen
+    if (request.method == 'POST'):  # redirect to the search screen
         search = request.form['search']
-        searchC= int(request.form['c'])
+        searchC = int(request.form['c'])
         searchP = int(request.form['p'])
-        results = [] #for centres
-        results2 = [] #for providers
+        results = []  # for centres
+        results2 = []  # for providers
 
         if (search == ""):
-            return render_template('search.html', empty = 1, c = searchC, p = searchP)
-
+            return render_template('search.html', empty=1, c=searchC, p=searchP)
 
         for centres in centreList:
-            #if (matchC(centres, search)): 
-            if (centres.matchCentre(search)):       
+            # if (matchC(centres, search)):
+            if (centres.matchCentre(search)):
                 results.append(centres)
                 for p in centres._providerList:
                     results2.append(p)
 
         for providers in providerList:
-            #if (matchP(providers, search)):
+            # if (matchP(providers, search)):
             if (providers.matchProvider(search)):
                 results2.append(providers)
                 for c in providers._working_centre:
                     results.append(c)
-                    
+
         results = list(set(results))
         results2 = list(set(results2))
-
 
         if (not searchC):
             results = []
         if (not searchP):
             results2 = []
         if (len(results) > 0 or len(results2) > 0):
-            return render_template('search.html', display = results, display2 = results2, s = search, c = searchC, p = searchP, results = 1)
+            return render_template('search.html', display=results, display2=results2, s=search, c=searchC, p=searchP,
+                                   results=1)
 
         else:
-            return render_template('search.html', s = search, results = 1, noDisplay = 1, c = searchC, p = searchP)
-   
-    return render_template('search.html', title = 'search', c = 1, p = 1)
+            return render_template('search.html', s=search, results=1, noDisplay=1, c=searchC, p=searchP)
 
-
+    return render_template('search.html', title='search', c=1, p=1)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -223,18 +232,15 @@ def register():
     # then validate said details after submission.
     form = RegistrationForm()
     if form.validate_on_submit():
-
-        # If entered details have passed validation, create the new user and
-        # add them to our user table.
-        user = User(username=form.username.data, email=form.email.data)
-        user.set_password(form.password.data)
-        db.session.add(user)
-        db.session.commit()
+        # If entered details have passed validation, create the new user and add them to our user table.
+        # -- TODO: We're assuming user is a patient, should update form to allow for role and workplace.
+        hsc.user_manager.add_user(form.username.data, form.email.data, form.password.data, role='Patient')
 
         flash('Woohoo, you did it! Redirecting to login screen.')
         return redirect(url_for('login'))
 
     return render_template('register.html', title='Register', form=form)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -255,19 +261,19 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
 
-        # If entered creds pass validation, check that user exists and that
-        # their password is correct.
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
+        # If credz pass validation, attempt to log the user in using them.
+        if hsc.user_manager.login_user(form.username.data, form.password.data):
+            # If we've arrived here, their credz are correct and they have been authenticated, redirect to the index.
+            return redirect(url_for('index'))
+
+        else:
+            # User has failed to log in, display an error and redirect to the login screen.
             flash('Error: Provided username or password is incorrect.')
             return redirect(url_for('login'))
 
-        # If we've arrived here, their credz are correct, authenticate them,
-        # then redirect back to the home page.
-        login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
-
+    # Render the login screen.
     return render_template('login.html', title='Sign In', form=form)
+
 
 @app.route('/logout')
 def logout():
@@ -278,15 +284,16 @@ def logout():
     If the user is not logged in, this method does nothing.
     """
 
-    logout_user()
+    hsc.user_manager.logout_user()
     return redirect(url_for('index'))
+
 
 @app.route('/appointments')
 def appointments():
     return render_template('appointments.html')
 
 
-@app.route('/currBooking', methods = ["GET", "POST"])
+@app.route('/currBooking', methods=["GET", "POST"])
 def currBooking():
     cancel = 0
     if (request.method == 'POST'):
@@ -297,8 +304,9 @@ def currBooking():
             s = request.form['search']
             result = request.form['result']
             provider = request.form['provider']
-        
-            return render_template('currBooking.html', user = currUser, cancel = cancel, l = len(currUser._appointment_list), view = view, c = c, p = p, search = s, result = result, provider = provider)
+
+            return render_template('currBooking.html', user=currUser, cancel=cancel, l=len(currUser._appointment_list),
+                                   view=view, c=c, p=p, search=s, result=result, provider=provider)
 
         name = request.form['name']
         time = request.form['time']
@@ -306,13 +314,10 @@ def currBooking():
         centre = request.form['centre']
         print(name + time + date + centre)
         for a in currUser._appointment_list:
-            if (time == a._start_time and date == a._date and centre == a._centre and name == a._health_care_provider._full_name):
-            
+            if (
+                                    time == a._start_time and date == a._date and centre == a._centre and name == a._health_care_provider._full_name):
                 currUser.removeAppointment(a)
-                cancel = 1        
-        #currUser.removeAppointment(app)
+                cancel = 1
+                # currUser.removeAppointment(app)
     length = len(currUser._appointment_list)
-    return render_template('currBooking.html', user = currUser, cancel = cancel, l = length)
-
-
-
+    return render_template('currBooking.html', user=currUser, cancel=cancel, l=length)
